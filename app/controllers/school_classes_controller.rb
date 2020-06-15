053c1270402af28497ca4b402b7a167e6ad8dfd1
Aaron Wilson <adub65@gmail.com>
2020-06-15 10:01:40 -0400

    basic form_for Rails

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,30 @@
+class SchoolClassesController < ApplicationController
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    show
+  end
+
+  def update
+    show
+    @school_class = SchoolClass.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+end
